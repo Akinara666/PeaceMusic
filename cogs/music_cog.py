f6d0825a8ada780f541ed64e053d54d8b2f2e3a1
Akinara666 <semen290612@gmail.com>
2026-02-02 00:01:25 +0300

    chore: Update Chrome version in the `USER_AGENT` string.

--- cogs/music_cog.py
+++ cogs/music_cog.py
@@ -120,7 +120,7 @@ def is_soundcloud_query(query: str) -> bool:
         )
     return False
 
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36'
 
 YTDL_OPTIONS = {
     'format': 'bestaudio/best',
